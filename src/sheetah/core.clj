82d0cbe0d485bb5dd516ce30fb09d52e8b44730c
jeremiegrodziski <jeremie@grodziski.com>
2021-04-14 12:08:24 +0200

    Add new operations: getting a single cell value () and get the named ranges of a sheet ()

--- src/sheetah/core.clj
+++ src/sheetah/core.clj
@@ -31,14 +31,48 @@
            receiver (-> (LocalServerReceiver$Builder.) (.setPort 8888) (.build))]
        (-> (AuthorizationCodeInstalledApp. flow receiver) (.authorize "user"))))))
 
-(defn sheets []
+(defn sheets
+  ^com.google.api.services.sheets.v4.Sheets []
   (let [transport (GoogleNetHttpTransport/newTrustedTransport)
         sheets (-> (Sheets$Builder. transport JSON_FACTORY (credentials transport))
                    (.setApplicationName APPLICATION_NAME)
                    (.build))]
     sheets))
 
-(defn cells-values [sheet-id sheet-name range major-dim]
+(defn alpha-uppercase? [s]
+  (re-matches #"[A-Z]+" s))
+
+(defn assert-row [x]
+  (if (instance? java.lang.String x)
+    (try (java.lang.Long/valueOf x) (catch NumberFormatException nfe (throw (ex-info "A row reference of the sheet in a string must be convert correctly to a long or integer" {:row x}))))
+    (when (not (or (instance? java.lang.Integer x) (instance? java.lang.Long x)))
+      (throw (ex-info "A row reference of the sheet must be an Integer or a long" {:row x})))))
+
+(defn cell-value
+  "return the value in a unique cell denoted by its column and row reference, e.g.: \"A\" and \"1\" for the A1 cell in the sheet"
+  [sheet-id sheet-name column row]
+  (when (not (alpha-uppercase? column)) (throw (ex-info "Column must be a string alpha with only uppercase character" {:column column})))
+  (assert-row row)
+  (-> (sheets)
+      (.spreadsheets)
+      (.values)
+      (.get sheet-id (str sheet-name "!" column row))
+      (.execute)
+      (.get "values")
+      (ffirst)))
+
+(defn named-ranges
+  "Return all the named ranges of the sheet, return a map with key a string of the name of the range and value of the range like {:endColumnIndex 15, :endRowIndex 237, :startColumnIndex 6, :startRowIndex 8} "
+  [sheet-id]
+  (let [raw-named-ranges (-> (sheets)
+                             (.spreadsheets)
+                             (.get sheet-id)
+                             (.execute)
+                             (.get "namedRanges"))]
+    (into {} (map (fn [{:strs [name range] :as namedRange}] [name (into {} (map (fn [[k v]] [(keyword k) v])) range)]) raw-named-ranges))))
+
+(defn cells-values
+  [sheet-id sheet-name range major-dim]
   (-> (sheets)
       (.spreadsheets)
       (.values)
